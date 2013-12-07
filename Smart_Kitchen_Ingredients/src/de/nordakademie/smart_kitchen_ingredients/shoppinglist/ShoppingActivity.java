a4a873ad694ba800bc0ee900d8d1438e493e167d
niels <niels@niels-ThinkPad-X61-Tablet>
2013-12-07 21:19:26 +0100

    - Wenn die Shoppingliste leer ist, wird darauf hingewiesen, dass man welche hinzufügen soll.

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/shoppinglist/ShoppingActivity.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/shoppinglist/ShoppingActivity.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import de.nordakademie.smart_kitchen_ingredients.IngredientsApplication;
 import de.nordakademie.smart_kitchen_ingredients.ModifyableList;
 import de.nordakademie.smart_kitchen_ingredients.R;
 import de.nordakademie.smart_kitchen_ingredients.businessobjects.ShoppingListItem;
+import de.nordakademie.smart_kitchen_ingredients.ingredient_management.ErfassungActivity;
 
-public class ShoppingActivity extends Activity implements ModifyableList {
+public class ShoppingActivity extends Activity implements ModifyableList,
+		OnClickListener {
 
 	private ListView shoppingListView;
+	private ImageButton btAddNewShoppingItem;
 	private IngredientsApplication app;
 
 	@Override
@@ -23,15 +31,25 @@ public class ShoppingActivity extends Activity implements ModifyableList {
 		setContentView(R.layout.shopping_layout);
 
 		app = (IngredientsApplication) getApplication();
+		btAddNewShoppingItem = (ImageButton) findViewById(R.id.addNewShoppingItem);
+		btAddNewShoppingItem.setOnClickListener(this);
 		shoppingListView = (ListView) findViewById(R.id.shoppingList);
+
+		TextView emptyView = new TextView(getApplicationContext());
+		emptyView.setText(R.string.emptyShoppingList);
+		shoppingListView.setEmptyView(findViewById(android.R.id.empty));
 
 		updateShoppingList();
 	}
 
 	private void updateShoppingList() {
-		ShoppingListAdapter adapter = new ShoppingListAdapter(
-				getApplicationContext(), this);
-		shoppingListView.setAdapter(adapter);
+		if (!getValues().isEmpty()) {
+			ShoppingListAdapter adapter = new ShoppingListAdapter(
+					getApplicationContext(), this);
+			shoppingListView.setAdapter(adapter);
+		} else {
+			shoppingListView.setAdapter(null);
+		}
 
 	}
 
@@ -62,6 +80,13 @@ public class ShoppingActivity extends Activity implements ModifyableList {
 	@Override
 	public List<ShoppingListItem> getShoppingItems() {
 		return app.getDbHelper().getAllShoppingItems();
+	}
+
+	@Override
+	public void onClick(View view) {
+		Intent intent = new Intent(getApplicationContext(),
+				ErfassungActivity.class);
+		startActivity(intent);
 	}
 
 }
