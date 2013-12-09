47368fb9230a88e534c8fc38487b8f3822ca202a
niels <niels@niels-ThinkPad-X61-Tablet>
2013-12-09 22:43:18 +0100

    - NetworkReceiver für die Netzwerkverbindung implementiert - CacheUpdateService angelegt (warte auf Interfaces)

---
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/localdata/CacheUpdaterService.java
@@ -0,0 +1,47 @@
+package de.nordakademie.smart_kitchen_ingredients.localdata;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import de.nordakademie.smart_kitchen_ingredients.IngredientsApplication;
+
+public class CacheUpdaterService extends Service {
+
+	private static final String TAG = CacheUpdaterService.class.getSimpleName();
+	IngredientsApplication app;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.i(TAG, "onCreate");
+		// TODO Cacheupdate
+
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(TAG, "onStartCommand");
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(TAG, "onDestroy");
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d(TAG, "onBind");
+		return null;
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		Log.d(TAG, "onUnBind");
+		return super.onUnbind(intent);
+
+	}
+
+}
---
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/localdata/NetworkReceiver.java
@@ -0,0 +1,25 @@
+package de.nordakademie.smart_kitchen_ingredients.localdata;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+public class NetworkReceiver extends BroadcastReceiver {
+
+	private static final String TAG = NetworkReceiver.class.getSimpleName();
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY,
+				false)) {
+			Log.d(TAG, "stopService");
+			context.stopService(new Intent(context, CacheUpdaterService.class));
+		} else {
+			Log.d(TAG, "startService");
+			context.startService(new Intent(context, CacheUpdaterService.class));
+		}
+	}
+
+}
