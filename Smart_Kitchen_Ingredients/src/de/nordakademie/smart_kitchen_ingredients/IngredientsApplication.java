6bc3994c84e7ed40612037dcd45d0d4cef06aaca
CaraFeelester <carafeelester@gmail.com>
2013-12-10 14:59:31 +0000

    Wenn der Network Reciver den CacheUpdater Service startet wird jetzt die Datenbank der Rezepte und Zutaten gecached.

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/IngredientsApplication.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/IngredientsApplication.java
@@ -2,7 +2,10 @@ package de.nordakademie.smart_kitchen_ingredients;
 
 import android.app.Application;
 import android.util.Log;
+import de.nordakademie.smart_kitchen_ingredients.localdata.ICacheRecipes;
+import de.nordakademie.smart_kitchen_ingredients.localdata.IRecipeData;
 import de.nordakademie.smart_kitchen_ingredients.localdata.IShoppingData;
+import de.nordakademie.smart_kitchen_ingredients.localdata.RecipeData;
 import de.nordakademie.smart_kitchen_ingredients.localdata.ShoppingData;
 
 public class IngredientsApplication extends Application {
@@ -11,18 +14,30 @@ public class IngredientsApplication extends Application {
 	public static final String PERMISSION = "de.nordakademie.smart_kitchen_ingredients.SHOPPING_LIST_CHANGING";
 	private final String TAG = IngredientsApplication.class.getSimpleName();
 	private IShoppingData shoppingDbHelper;
+	private ICacheRecipes cacheRecipesHelper;
+	private IRecipeData getRecipesHelper;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
 		shoppingDbHelper = new ShoppingData(getApplicationContext());
+		cacheRecipesHelper = new RecipeData(getApplicationContext());
+		getRecipesHelper = new RecipeData(getApplicationContext());
 
 		Log.i(TAG, "Application started");
 	}
 
 	public IShoppingData getDbHelper() {
 		return shoppingDbHelper;
+	}
+	
+	public ICacheRecipes getCacheDbHelper(){
+		return cacheRecipesHelper;
+	}
+	
+	public IRecipeData getRecipesFromCacheHelper(){
+		return getRecipesHelper;
 	}
 
 }
--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/localdata/CacheUpdaterService.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/localdata/CacheUpdaterService.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import de.nordakademie.smart_kitchen_ingredients.IngredientsApplication;
+import de.nordakademie.smart_kitchen_ingredients.onlinedata.Connector;
+import de.nordakademie.smart_kitchen_ingredients.onlinedata.IServerHandler;
+import de.nordakademie.smart_kitchen_ingredients.onlinedata.ServerHandler;
 
 public class CacheUpdaterService extends Service {
 
@@ -15,8 +18,12 @@ public class CacheUpdaterService extends Service {
 	public void onCreate() {
 		super.onCreate();
 		Log.i(TAG, "onCreate");
-		// TODO Cacheupdate
-
+		
+		IServerHandler handler = new ServerHandler(new Connector());
+		ICacheRecipes cacher = new RecipeData(getApplicationContext());
+		
+		cacher.cacheAllIngredients(handler.getIngredientListFromServer());
+		cacher.cacheAllRecipes(handler.getRecipeListFromServer());
 	}
 
 	@Override
