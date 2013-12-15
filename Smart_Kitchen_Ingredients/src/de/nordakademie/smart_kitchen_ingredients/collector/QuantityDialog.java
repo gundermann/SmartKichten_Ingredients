e814b2280f9298b3dff51dea78daf2f85208fae3
frederic oppermann <frederic.oppermann@localhost>
2013-12-15 23:50:28 +0100

    quantity picker layout appears.

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/QuantityDialog.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/QuantityDialog.java
@@ -1,10 +1,11 @@
 package de.nordakademie.smart_kitchen_ingredients.collector;
 
-import de.nordakademie.smart_kitchen_ingredients.R;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
+import de.nordakademie.smart_kitchen_ingredients.R;
 
 /**
  * @author frederic.oppermann
@@ -14,9 +15,12 @@ import android.support.v4.app.DialogFragment;
 public class QuantityDialog extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		View view = getActivity().getLayoutInflater().inflate(
+				R.layout.number_picker_layout, null);
+
 		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(
 				getActivity());
-		dialogBuilder.setTitle(R.string.quantityDialogTitle);
+		dialogBuilder.setTitle(R.string.quantityDialogTitle).setView(view);
 		return dialogBuilder.create();
 	}
 }
