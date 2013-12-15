1f1e3496ac74757cdb61749e8bfb60b07aac2694
Frederic Oppermann <Frederic Oppermann@FredericOpperma>
2013-12-15 17:32:16 +0100

    recipes can be filtered

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/RecipeCollectorActivity.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/RecipeCollectorActivity.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import de.nordakademie.smart_kitchen_ingredients.R;
+import de.nordakademie.smart_kitchen_ingredients.businessobjects.IIngredient;
 import de.nordakademie.smart_kitchen_ingredients.businessobjects.IRecipe;
 
 public class RecipeCollectorActivity extends AbstractCollectorActivity<IRecipe> {
@@ -31,6 +33,13 @@ public class RecipeCollectorActivity extends AbstractCollectorActivity<IRecipe> 
 	@Override
 	public void update(AsyncTask<Void, Void, List<IRecipe>> task) {
 		super.update(task);
+		setNewAdapter(new ArrayAdapter<IRecipe>(getApplicationContext(),
+				R.layout.list_view_entry, getElementsToShow()));
+	}
+
+	@Override
+	public void afterTextChanged(Editable s) {
+		super.afterTextChanged(s);
 		setNewAdapter(new ArrayAdapter<IRecipe>(getApplicationContext(),
 				R.layout.list_view_entry, getElementsToShow()));
 	}
