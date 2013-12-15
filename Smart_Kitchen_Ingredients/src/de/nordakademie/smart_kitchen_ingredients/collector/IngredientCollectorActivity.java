43577f74dbda0f1cd8cbeb4e71144b5437b5042b
Frederic Oppermann <Frederic Oppermann@FredericOpperma>
2013-12-15 17:56:58 +0100

    code changed to adapter factory

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/IngredientCollectorActivity.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/IngredientCollectorActivity.java
@@ -6,7 +6,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import de.nordakademie.smart_kitchen_ingredients.R;
 import de.nordakademie.smart_kitchen_ingredients.businessobjects.IIngredient;
@@ -34,7 +33,7 @@ public class IngredientCollectorActivity extends
 	@Override
 	public void update(AsyncTask<Void, Void, List<IIngredient>> task) {
 		super.update(task);
-		setNewAdapter(new ArrayAdapter<IIngredient>(getApplicationContext(),
+		setNewAdapter(adapterFactory.createAdapter(getApplicationContext(),
 				R.layout.list_view_entry, getElementsToShow()));
 	}
 
--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/RecipeCollectorActivity.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/collector/RecipeCollectorActivity.java
@@ -6,13 +6,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import de.nordakademie.smart_kitchen_ingredients.R;
 import de.nordakademie.smart_kitchen_ingredients.businessobjects.IRecipe;
 
 public class RecipeCollectorActivity extends AbstractCollectorActivity<IRecipe> {
 	private Button showIngredientsButton;
+	private IAdapterFactory<IRecipe> adapterFactory = new AdapterFactory<IRecipe>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +32,14 @@ public class RecipeCollectorActivity extends AbstractCollectorActivity<IRecipe> 
 	@Override
 	public void update(AsyncTask<Void, Void, List<IRecipe>> task) {
 		super.update(task);
-		setNewAdapter(new ArrayAdapter<IRecipe>(getApplicationContext(),
+		setNewAdapter(adapterFactory.createAdapter(getApplicationContext(),
 				R.layout.list_view_entry, getElementsToShow()));
 	}
 
 	@Override
 	public void afterTextChanged(Editable s) {
 		super.afterTextChanged(s);
-		setNewAdapter(new ArrayAdapter<IRecipe>(getApplicationContext(),
+		setNewAdapter(adapterFactory.createAdapter(getApplicationContext(),
 				R.layout.list_view_entry, getElementsToShow()));
 	}
 }
