6dff95be649ba7a9a4a94c5ce56129279176a9aa
Frauke Trautmann <Frauke.trautmann@nordakademie.de>
2013-12-18 23:15:00 +0100

    Einkaufsliste erstellen: Nutzernachricht wenn Nutzer keinen Namen vergeben hat und trotzdem bestätigt.

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/dialog/InsertNameDialog.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/dialog/InsertNameDialog.java
@@ -12,6 +12,7 @@ import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import de.nordakademie.smart_kitchen_ingredients.IngredientsApplication;
 import de.nordakademie.smart_kitchen_ingredients.R;
 
 /**
@@ -40,9 +41,16 @@ public class InsertNameDialog extends DialogFragment {
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						Log.d(TAG, inputField.getText().toString());
-						dialogListener.onPositiveFinishedDialog(inputField
-								.getText().toString());
+						if (!inputField.getText().toString().equals("")) {
+							Log.d(TAG, inputField.getText().toString());
+							dialogListener.onPositiveFinishedDialog(inputField
+									.getText().toString());
+						} else {
+							((IngredientsApplication) getActivity()
+									.getApplication())
+									.informUser(R.string.userInformFieldIsEmpty);
+
+						}
 					}
 				});
 		return dialogBuilder.create();
