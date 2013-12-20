03f13c60adb198cb8e44dcd76a63a3c3b2b4c80a
frederic oppermann <frederic.oppermann@localhost>
2013-12-20 23:54:22 +0100

    bug fix. final commit. diese version hier liegt auch im CVS zur abgabe.
    \m/

--- Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/stock/StockPrefsActivity.java
+++ Smart_Kitchen_Ingredients/src/de/nordakademie/smart_kitchen_ingredients/stock/StockPrefsActivity.java
@@ -7,6 +7,7 @@ import android.text.InputFilter;
 import android.text.InputType;
 import de.nordakademie.smart_kitchen_ingredients.R;
 import de.nordakademie.smart_kitchen_ingredients.businessobjects.Unit;
+
 /**
  * @author Frauke Trautmann
  */
@@ -24,7 +25,7 @@ public class StockPrefsActivity extends PreferenceActivity {
 		for (Unit unit : Unit.values()) {
 			EditTextPreference editTextPreference = new EditTextPreference(this);
 			editTextPreference.setKey(unit.toString());
-			editTextPreference.setTitle(TITLE + unit);
+			editTextPreference.setTitle(TITLE + unit.toLongString());
 			editTextPreference.getEditText().setInputType(
 					InputType.TYPE_CLASS_NUMBER);
 			InputFilter[] filters = new InputFilter[1];
